2695edc261e33cbf4d378a3bb753e5077761d433
GraceMattern <gracefmattern@gmail.com>
2022-02-23 10:01:12 -0500

    add server unit test, character sprites

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var connect = require('connect');
 var http = require('http');
 
 var path = "";
-
 var app = connect().use(express.static(__dirname + path));
+
 http.createServer(app).listen(8080);
 console.log('Server will be running on http://localhost:8080');
---
+++ tests/server.test.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+describe('Server', () => {
+  let server;
+
+  beforeAll(done => {
+    server = http.createServer((req, res) => {
+      res.write('ok');
+      res.end();
+    });
+    server.listen(done);
+  });
+
+  afterAll(done => {
+    server.close(done);
+  });
+
+  test('my test', async () => {});
+});
