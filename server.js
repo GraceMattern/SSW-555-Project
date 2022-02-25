39eaebd6d2b6a0321efeeab0babfcf7fe580c19e
Dhruveel Doshi <34346668+dhruveeldoshi@users.noreply.github.com>
2022-02-25 15:37:48 -0500

    server running, index.html created

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var connect = require('connect');
-var http = require('http');
+const express = require("express");
+// const connect = require("connect");
+const app = express();
 
-var path = "";
-var app = connect().use(express.static(__dirname + path));
+// const http = require("http");
 
-http.createServer(app).listen(8080);
-console.log('Server will be running on http://localhost:8080');
+const path = __dirname + "/index.html";
+
+app.use("/", async (req, res) => {
+  try {
+    res.sendFile(path);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Server will be running on http://localhost:8080");
+});
+// http.createServer(app).listen(8080);
+// console.log("Server will be running on http://localhost:8080");
