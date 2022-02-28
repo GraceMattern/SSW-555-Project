2a8373f76052ec96ee62815876ba5e90106089cb
Dhruveel Doshi <34346668+dhruveeldoshi@users.noreply.github.com>
2022-02-28 02:34:06 -0500

    Player Name Gathering and Quit Butto (Initial)

--- Overworld.js
+++ Overworld.js
@@ -3,33 +3,33 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    // storing playername inside this.playerName
+    this.playerName =
+      config.playerName.length < 1 ? "DefaultName" : config.playerName;
   }
- 
+
   init() {
     const bg = new Image();
     bg.onload = () => {
-      this.ctx.drawImage(bg,0,0)
+      this.ctx.drawImage(bg, 0, 0);
     };
     bg.src = "/assets/images/maps/map_3.png";
- 
+
     //Game Objects
     const protag = new GameObject({
       x: 5,
       y: 6,
-    })
+    });
     const npc1 = new GameObject({
-     x: 0,
-     y: 0,
-     src: "/assets/images/characters/sprite02.png"
-   })
- 
-   // draw game obj
-   setTimeout(() => {
-     protag.sprite.draw(this.ctx);
-     npc1.sprite.draw(this.ctx);
-   }, 200)
- 
- 
+      x: 0,
+      y: 0,
+      src: "/assets/images/characters/sprite02.png",
+    });
+
+    // draw game obj
+    setTimeout(() => {
+      protag.sprite.draw(this.ctx);
+      npc1.sprite.draw(this.ctx);
+    }, 200);
   }
- 
- }
+}
--- init.js
+++ init.js
@@ -1,8 +1,14 @@
 (function () {
-
   const overworld = new Overworld({
-    element: document.querySelector(".game-container")
+    element: document.querySelector(".game-container"),
+    playerName: prompt("Enter the name of the player."),
   });
   overworld.init();
-  
+
+  const quitButton = document.createElement("button");
+  quitButton.innerHTML = "quit";
+  quitButton.onclick = () => {
+    alert("Quit successfully- initial");
+  };
+  document.body.appendChild(quitButton);
 })();
