b7ad701acc3ddd2e0cc0c21a1c6600c60c8f1f85
prathyekareddy <prathyekaanugu@gmail.com>
2022-03-22 19:59:47 -0400

    Inventory added

--- Fruits.js
+++ Fruits.js
@@ -8,7 +8,7 @@ window.Fruitss = {
   "A1": {
     name: "Red Apple",
     description: "Picked an Apple",
-    type: PizzaTypes.spicy,
+    type: FruitTypes.apple,
     src: "/assets/images/food/Apple.png",
     icon: "/assets/images/food/Apple.png",
     actions: [ "picked" ],
@@ -16,7 +16,7 @@ window.Fruitss = {
   "SB1": {
     name: "Strawberry",
     description: "Picked a Strawberry",
-    type: PizzaTypes.spicy,
+    type: FruitTypes.strawberry,
     src: "/assets/images/food/Strawberry.png",
     icon: "/assets/images/food/Strawberry.png",
     actions: [ "picked" ],
--- OverworldEvent.js
+++ OverworldEvent.js
@@ -80,6 +80,22 @@ class OverworldEvent {
     this.map.overworld.startMap(window.OverworldMaps[this.event.map]);
     resolve();
   }
+  //This is to flag if the user has visited the item
+  addStoryFlag(resolve) {
+    return new Promise(resolve => {
+      this[this.event.this](resolve)
+    })
+  }
+  //This code is to push the items collected to an inventory
+  CraftingMenu(resolve) {
+    const menu = new this.CraftingMenu({
+      fruits: this.event.fruits,
+      onComplete: () => {
+        resolve();
+      }
+    })
+    menu.init(document.querySelector(".game-container"))
+  }
 
   init() {
     return new Promise((resolve) => {
