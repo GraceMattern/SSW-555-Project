00c309f7d958044f46c698f4c4544ec54084442c
GraceMattern <56277939+GraceMattern@users.noreply.github.com>
2022-03-21 22:16:59 -0400

    Update GameObject.js

--- GameObject.js
+++ GameObject.js
@@ -1,5 +1,8 @@
 class GameObject {
   constructor(config) {
+    this.id = null;
+    this.isMounted = false;
+
     this.x = config.x || 0;
     this.y = config.y || 0;
     this.direction = config.direction || "down";
@@ -7,12 +10,50 @@ class GameObject {
       gameObject: this,
       src: config.src || "assets/images/characters/sprite01.png",
     });
+
+    this.behaviorLoop = config.behaviorLoop || 0;
+    this.behaviorLoopIndex = 0;
+
+    this.talking = config.talking || [];
   }
 
-  update() {
+  mount(map) {
+    console.log("mounting!");
+    this.isMounted = true;
+    map.addWall(this.x, this.y);
 
+    setTimeout(() => {
+      this.doBehaviorEvent(map);
+    }, 10);
+  }
+
+  update() {}
+
+  async doBehaviorEvent(map) {
+    //Don't do anything if there is a more important cutscene or I don't have config to do anything
+    //anyway.
+    if (map.isCutscenePlaying || this.behaviorLoop.length === 0) {
+      return;
+    }
+
+    let eventConfig = this.behaviorLoop[this.behaviorLoopIndex];
+    eventConfig.who = this.id;
+
+    const eventHandler = new OverworldEvent({ map, event: eventConfig });
+    await eventHandler.init();
+
+    //Setting the next event to fire
+    this.behaviorLoopIndex += 1;
+    if (this.behaviorLoopIndex === this.behaviorLoop.length) {
+      this.behaviorLoopIndex = 0;
+    }
+
+    //Do it again!
+    this.doBehaviorEvent(map);
   }
 }
+
+// =======================================
 
 class Person extends GameObject {
   constructor(config) {
@@ -22,19 +63,69 @@ class Person extends GameObject {
     this.isPlayerControlled = config.isPlayerControlled || false;
 
     this.directionUpdate = {
-      "up": ["y", -1],
-      "down": ["y", 1],
-      "left": ["x", -1],
-      "right": ["x", 1],
-    }
+      up: ["y", -1],
+      down: ["y", 1],
+      left: ["x", -1],
+      right: ["x", 1],
+    };
   }
 
   update(state) {
     this.updatePosition();
 
-    if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
+    if (
+      this.isPlayerControlled &&
+      this.movingProgressRemaining === 0 &&
+      state.arrow
+    ) {
       this.direction = state.arrow;
       this.movingProgressRemaining = 16;
+    }
+    // if (this.movingProgressRemaining > 0) {
+    //   this.updatePosition();
+    // } else {
+    //   //More cases for starting to walk will come here
+    //   //
+    //   //
+
+    //   //Case: We're keyboard ready and have an arrow pressed
+    //   if (
+    //     !state.map.isCutscenePlaying &&
+    //     this.isPlayerControlled &&
+    //     state.arrow
+    //   ) {
+    //     this.startBehavior(state, {
+    //       type: "walk",
+    //       direction: state.arrow,
+    //     });
+    //   }
+    //   this.updateSprite(state);
+    // }
+  }
+  startBehavior(state, behavior) {
+    //Set character direction to whatever behavior has
+    this.direction = behavior.direction;
+
+    if (behavior.type === "walk") {
+      //Stop here if space is not free
+      // if (state.map.isSpaceTaken(this.x, this.y, this.direction)) {
+      //   behavior.retry &&
+      //     setTimeout(() => {
+      //       this.startBehavior(state, behavior);
+      //     }, 10);
+      //   return;
+    }
+
+    //Ready to walk!
+    // state.map.moveWall(this.x, this.y, this.direction);
+    this.movingProgressRemaining = 16;
+    // this.updateSprite(state);
+    if (behavior.type === "stand") {
+      setTimeout(() => {
+        utils.emitEvent("PersonStandComplete", {
+          whoId: this.id,
+        });
+      }, behavior.time);
     }
   }
 
@@ -43,6 +134,21 @@ class Person extends GameObject {
       const [property, change] = this.directionUpdate[this.direction];
       this[property] += change;
       this.movingProgressRemaining -= 1;
+
+      if (this.movingProgressRemaining === 0) {
+        //We finished the walk!
+        utils.emitEvent("PersonWalkingComplete", {
+          whoId: this.id,
+        });
+      }
     }
+  }
+
+  updateSprite() {
+    // if (this.movingProgressRemaining > 0) {
+    //   this.sprite.setAnimation("walk-" + this.direction);
+    //   return;
+    // }
+    // this.sprite.setAnimation("idle-" + this.direction);
   }
 }
