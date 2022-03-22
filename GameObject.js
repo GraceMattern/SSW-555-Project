a7a995fc1d526aef8c8eb6902b75a1a9746d0164
GraceMattern <56277939+GraceMattern@users.noreply.github.com>
2022-03-21 22:15:36 -0400

    Update GameObject.js

--- GameObject.js
+++ GameObject.js
@@ -1,8 +1,5 @@
 class GameObject {
   constructor(config) {
-    this.id = null;
-    this.isMounted = false;
-
     this.x = config.x || 0;
     this.y = config.y || 0;
     this.direction = config.direction || "down";
@@ -10,46 +7,10 @@ class GameObject {
       gameObject: this,
       src: config.src || "assets/images/characters/sprite01.png",
     });
-
-    this.behaviorLoop = config.behaviorLoop || 0;
-    this.behaviorLoopIndex = 0;
-
-    this.talking = config.talking || [];
   }
 
-  mount(map) {
-    console.log("mounting!");
-    this.isMounted = true;
-    map.addWall(this.x, this.y);
+  update() {
 
-    setTimeout(() => {
-      this.doBehaviorEvent(map);
-    }, 10);
-  }
-
-  update() {}
-  
-  async doBehaviorEvent(map) {
-    //Don't do anything if there is a more important cutscene or I don't have config to do anything
-    //anyway.
-    if (map.isCutscenePlaying || this.behaviorLoop.length === 0) {
-      return;
-    }
-
-    let eventConfig = this.behaviorLoop[this.behaviorLoopIndex];
-    eventConfig.who = this.id;
-
-    const eventHandler = new OverworldEvent({ map, event: eventConfig });
-    await eventHandler.init();
-
-    //Setting the next event to fire
-    this.behaviorLoopIndex += 1;
-    if (this.behaviorLoopIndex === this.behaviorLoop.length) {
-      this.behaviorLoopIndex = 0;
-    }
-
-    //Do it again!
-    this.doBehaviorEvent(map);
   }
 }
 
@@ -70,7 +31,6 @@ class Person extends GameObject {
 
   update(state) {
     this.updatePosition();
-    this.updateSprite(state);
 
     if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
       this.direction = state.arrow;
@@ -83,16 +43,6 @@ class Person extends GameObject {
       const [property, change] = this.directionUpdate[this.direction];
       this[property] += change;
       this.movingProgressRemaining -= 1;
-    }
-  }
-
-  updateSprite(state) {
-    if (this.isPlayerControlled && this.movingProgressRemaining === 0 && !state.arrow) {
-      this.sprite.setAnimation("idle-"+this.direction);
-      return;
-    }
-    if (this.movingProgressRemaining > 0) {
-      this.sprite.setAnimation("walk-"+this.direction);
     }
   }
 }
