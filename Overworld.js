421cc15a1a39d21df49444b25d4831c706a02e4f
GraceMattern <56277939+GraceMattern@users.noreply.github.com>
2022-03-21 21:36:30 -0400

    Update Overworld.js
    Cutscene testing

--- Overworld.js
+++ Overworld.js
@@ -1,6 +1,8 @@
 class OverworldMap {
   constructor(config) {
     this.gameObjects = config.gameObjects;
+    this.walls = config.walls || {};
+    this.cutsceneSpaces = config.cutsceneSpaces || {};
 
     this.lowerImage = new Image();
     this.lowerImage.src = config.lowerSrc;
@@ -28,6 +30,15 @@ class OverworldMap {
     );
   }
 
+  mountObjects() {
+    Object.keys(this.gameObjects).forEach((key) => {
+      //TODO: determine if this object should actually mount
+      let object = this.gameObjects[key];
+      object.id = key;
+      object.mount(this);
+    });
+  }
+
   async startCutscene(events) {
     this.isCutscenePlaying = true;
 
@@ -41,6 +52,38 @@ class OverworldMap {
 
     this.isCutscenePlaying = false;
   }
+
+  checkForActionCutscene() {
+    const hero = this.gameObjects["protag"];
+    const nextCoords = utils.nextPosition(hero.x, hero.y, hero.direction);
+    const match = Object.values(this.gameObjects).find((object) => {
+      return `${object.x},${object.y}` === `${nextCoords.x},${nextCoords.y}`;
+    });
+    if (!this.isCutscenePlaying && match && match.talking.length) {
+      this.startCutscene(match.talking[0].events);
+    }
+  }
+
+  checkForFootstepCutscene() {
+    const hero = this.gameObjects["protag"];
+    const match = this.cutsceneSpaces[`${hero.x},${hero.y}`];
+
+    if (!this.isCutscenePlaying && match) {
+      this.startCutscene(match[0].events);
+    }
+  }
+
+  addWall(x, y) {
+    this.walls[`${x},${y}`] = true;
+  }
+  removeWall(x, y) {
+    delete this.walls[`${x},${y}`];
+  }
+  moveWall(wasX, wasY, direction) {
+    this.removeWall(wasX, wasY);
+    const { x, y } = utils.nextPosition(wasX, wasY, direction);
+    this.addWall(x, y);
+  }
 }
 
 window.OverworldMaps = {
@@ -48,16 +91,47 @@ window.OverworldMaps = {
     lowerSrc: "assets/images/maps/map_3.png",
     // upperSrc: "", // TODO
     gameObjects: {
-      npc1: new GameObject({
-        x: utils.withGrid(0),
-        y: utils.withGrid(0),
-        src: "/assets/images/characters/sprite02.png",
-      }),
       protag: new Person({
         isPlayerControlled: true,
         x: utils.withGrid(5),
         y: utils.withGrid(6),
       }),
+      npc1: new Person({
+        x: utils.withGrid(0),
+        y: utils.withGrid(0),
+        src: "/assets/images/characters/sprite02.png",
+        // behaviorLoop: [
+        //   // { type: "stand", direction: "left", time: 800 },
+        //   // { type: "stand", direction: "right", time: 1200 },
+        // ],
+        talking: [
+          {
+            events: [
+              {
+                type: "textMessage",
+                text: "Welcome to The Giving Garden",
+                faceHero: "npc1",
+              },
+              { type: "textMessage", text: "Start by collecting some herbs!" },
+            ],
+          },
+        ],
+      }),
+    },
+    cutsceneSpaces: {
+      // [utils.asGridCoord(7, 8)]: [
+      //   {
+      //     events: [
+      //       {
+      //         who: "npc1",
+      //         type: "stand",
+      //         direction: "left",
+      //         time: 500,
+      //       },
+      //       { type: "textMessage", text: "Pick Herbs, Veggies and Fruits" },
+      //     ],
+      //   },
+      // ],
     },
   },
 };
